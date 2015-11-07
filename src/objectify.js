1242ff63f48589ce3c3e57178f53fd1a410fe74f
jkrayer <jameskrayer@yahoo.com>
2015-11-06 23:37:35 -0500

    Add js function with jQuery dependency

---
+++ src/objectify.js
@@ -0,0 +1,33 @@
+/**
+ * Form data into javascript object
+ * @author Jim Krayer <jameskrayer@yahoo.com>
+ * @version 0.0.2
+ * @param  {object} $form      jQuery form object
+ * @param  {array}  exclusions a list of field to exclude in the return, must
+ *                             match field name attribute
+ * @param  {object} sanitize   object of sanitize functions to run, must match
+ *                             field name attribute
+ * @return {object}            object of form data
+ */
+function objectifyForm ($form, exclusions, sanitize) {
+  var formData = $form.serializeArray();
+  var obj = {};
+  var key;
+  var name;
+
+  exclusions = exclusions || [];
+  sanitize = sanitize || false;
+
+  for (key in formData) {
+    name = formData[key].name;
+    if (exclusions.indexOf(name) > -1) {
+      continue;
+    }
+    if (sanitize && sanitize.hasOwnProperty(name)) {
+      obj[name] = sanitize[name](formData[key].value);
+      continue;
+    }
+    obj[name] = formData[key].value;
+  }
+  return obj;
+}
