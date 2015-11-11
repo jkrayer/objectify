4dd664544204a88ad1d26dfc4318a2287fff45f7
jkrayer <jkr@trustpilot.com>
2015-11-11 16:17:58 -0500

    Debug js objectify

--- src/objectify.js
+++ src/objectify.js
@@ -28,6 +28,7 @@ function objectify (form, exclusions, sanitize) {
   (function getAllInputs (object) {
     var key;
     for (key in object) {
+      if (object[key].nodeType !== 1) { continue; } //skip non-element nodes
       if (inputTags.test(object[key].nodeName)) {
         formInputs.push(object[key]);
         continue;
@@ -47,7 +48,7 @@ function objectify (form, exclusions, sanitize) {
       && inputTags.test(input.nodeName)
       && !/^(?:submit|button|image|reset|file)$/i.test(type)
       && (input.checked || !/^(?:checkbox|radio)$/i.test(type))
-      && exclusions.indexOf(name) > -1
+      && exclusions.indexOf(name) === -1
   });
 
   //sanitize
