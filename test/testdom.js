fd85819cf429f39139c6558286443d76bd1dd453
jkrayer <jkr@trustpilot.com>
2015-11-13 17:07:55 -0500

    Add test files

---
+++ test/testdom.js
@@ -0,0 +1,10 @@
+var jsdom = require('jsdom').jsdom;
+
+module.exports = function(markup) {
+  if (typeof document !== 'undefined') return;
+  global.document = jsdom(markup || '');
+  global.window = document.defaultView;
+  global.navigator = {
+    userAgent: 'node.js'
+  };
+};
